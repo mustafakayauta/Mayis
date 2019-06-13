a5089f8c844a0837f143fef77f3a4e95a726276a
Mustafa Kaya <mustafakayauta@gmail.com>
2019-06-13 15:59:51 -0500

    Hello git

--- src/Mayis5/Horse.java
+++ src/Mayis5/Horse.java
@@ -1,41 +1,11 @@
 package Mayis5;
 
 public class Horse {
-
-		String breed;
-		int age;
-		String color;
-		double height;
-		
-
-	public static void main (String [] args) {
-		
-		
-		Horse h1=new Horse();
-		
-		
-		h1.breed="arabian";
-		h1.age=3;
-		h1.color="red";
-		h1.height=4.3;
-		
-		Horse h2=new Horse();
-		
-
-		h2.breed="turk";
-		h2.age=1;
-		h2.color="greem";
-		h2.height=55.1;
-		
-		
-		System.out.println(h1.color);
-		System.out.println(h2.height);
-		
-		h1.color="brown";  // updated
-		
-		System.out.println(h1.color);
-		System.out.println(h2.color);
-		
+public static void main(String[] args) {
+	
+System.out.println("hello world");
+	
+	
 	}
 
 }
